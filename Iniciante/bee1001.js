a926833ba2b0f9aa2889143421a82c1dcb8ad8c6
Italo1994 <italohortiz2015@gmail.com>
2022-02-23 01:57:03 -0300

    Enviando a solução dos problemas 1001 e 1002

---
+++ Iniciante/bee1001.js
@@ -0,0 +1,5 @@
+let A=10, B=9, X=0;
+
+X = A + B; // soma os valores armazenados nas variáveis A e B e os atribui a X
+
+console.log(X+"\n");
---
+++ Iniciante/bee1002.js
@@ -0,0 +1,8 @@
+let raio=0, pi = 3.14159, area=0; // inicializando as variáveis
+
+raio = 100.64;
+//area = Math.ceil( Math.toFixed( pi * (raio ** 2)) );
+area = pi * (raio ** 2);
+area = area.toFixed(4);
+
+console.log("A=" + area + "\n");
